b1291e6df7bc573c7844660e0825d47d5c1f24f4
Ralph Burleson <ralph.burleson@posigent.com>
2022-05-30 14:13:25 +0200

    Travel changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,55 +10,91 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.orangeAccent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.coffee_rounded, color: Colors.white),
-                SizedBox(width: 10),
-                Text(
-                  "Welcome",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Bembo',
-                    fontSize: 24,
+        backgroundColor: Colors.pink,
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset("images/plane.png"),
                   ),
-                ),
-              ],
-            ),
-            Center(
-              child: Image.asset("images/welcome.png"),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print('Sign In');
-              },
-              child: Text('Sign In'),
-              style: OutlinedButton.styleFrom(
-                side: BorderSide(
-                  color: Colors.white,
-                  width: 2.0,
-                ),
-                primary: Colors.white,
-                minimumSize: Size(200, 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.coffee_rounded, color: Colors.white),
+                      SizedBox(width: 10),
+                      Text(
+                        "TRAVEL APP",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Bembo',
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 7,
+                        ),
+                      ),
+                    ],
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      print('Stays');
+                    },
+                    child: Text('Stays'),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                      primary: Colors.white,
+                      minimumSize: Size(200, 40),
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      print('Flights');
+                    },
+                    child: Text('Flights'),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                      primary: Colors.white,
+                      minimumSize: Size(200, 40),
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      print('Cars');
+                    },
+                    child: Text('Cars'),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                      primary: Colors.white,
+                      minimumSize: Size(200, 40),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  TextButton(
+                    onPressed: () {
+                      print('Register');
+                    },
+                    child: Text('All-Inclusive Vacations'),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      primary: Colors.orange,
+                      minimumSize: Size(200, 40),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 5),
-            TextButton(
-              onPressed: () {
-                print('Register');
-              },
-              child: Text('Subscribe'),
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.white,
-                primary: Colors.orange,
-                minimumSize: Size(200, 40),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
